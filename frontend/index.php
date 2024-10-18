422edeaa0fc543ec9a1c95984e07d8188c4bfe79
Colornos <rivas432@hotmail.com>
2024-10-18 16:00:53 -0500

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -170,7 +170,7 @@ $primary_domain = trim(shell_exec("hostname -I | awk '{print $1}'"));
             <div class="navbar-header">
                 <ul class="nav navbar-nav flex-row">
                     <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
-                    <li class="nav-item"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="x" src="app-assets/images/logo/logo.png">
+                    <li class="nav-item"><a class="navbar-brand" href="index.php"><img class="brand-logo" alt="x" src="app-assets/images/logo/logo.png">
                             <h3 class="brand-text">xPanel</h3>
                         </a></li>
                     <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="la la-ellipsis-v"></i></a></li>
@@ -181,8 +181,8 @@ $primary_domain = trim(shell_exec("hostname -I | awk '{print $1}'"));
                     <ul class="nav navbar-nav float-right">
                         <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown"><span class="avatar avatar-online"><img src="app-assets/images/portrait/small/avatar-s-19.png" alt="avatar"><i></i></span><span class="user-name"><?php echo $current_user; ?></span></a>
                             <div class="dropdown-menu dropdown-menu-right">
-                                <a class="dropdown-item" href="user-profile.html"><i class="ft-user"></i> Edit Profile</a>
-                                <div class="dropdown-divider"></div><a class="dropdown-item" href="login-with-bg-image.html"><i class="ft-power"></i> Logout</a>
+                                <a class="dropdown-item" href="#"><i class="ft-user"></i> Edit Profile</a>
+                                <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="ft-power"></i> Logout</a>
                             </div>
                         </li>
                     </ul>
