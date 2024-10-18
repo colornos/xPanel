6d6de7950ffad73f4bb7ed9679d5df0c3c98d2eb
Colornos <rivas432@hotmail.com>
2024-10-18 14:27:29 -0500

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -167,7 +167,7 @@ $primary_domain = trim(shell_exec("hostname -I | awk '{print $1}'"));
             <div class="navbar-header">
                 <ul class="nav navbar-nav flex-row">
                     <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
-                    <li class="nav-item"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="modern admin logo" src="app-assets/images/logo/logo.png">
+                    <li class="nav-item"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="x" src="app-assets/images/logo/logo.png">
                             <h3 class="brand-text">xPanel</h3>
                         </a></li>
                     <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="la la-ellipsis-v"></i></a></li>
